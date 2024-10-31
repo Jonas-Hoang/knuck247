1cdd41b3251f74e9969c57a4f6e43b2c6d784af1
Jonas-Hoang <66858257+Jonas-Hoang@users.noreply.github.com>
2024-10-31 20:55:14 +0700

    add section

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div class="p-10 page bg-white overflow-hidden">
+      <section class="homepage-section h-[200px] d-xl-flex position-relative bg-red-300 home-section homepage-hero-section">
+      
+      </section>
       <div class="synchronized-scroll horizontal">
         <i class="navigation-links align-items-center justify-content-between py-0-5 py-sm-1 px-0-625 px-sm-1-25 px-xl-2 py-xl-1 disabled d-none d-xl-flex">
           <NuxtLink to="/">Home </NuxtLink>
