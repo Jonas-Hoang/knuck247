1145e0fdb5db58d3384505cecabb77d6e36bc39f
Jonas-Hoang <66858257+Jonas-Hoang@users.noreply.github.com>
2024-10-31 20:46:59 +0700

    add package scss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-    <div class="p-10">
-      <i class="navigation-links align-items-center justify-content-between py-0-5 py-sm-1 px-0-625 px-sm-1-25 px-xl-2 py-xl-1 disabled d-none d-xl-flex">
-        <NuxtLink to="/">Home </NuxtLink>
-      </i>
-      <i class="navigation-links align-items-center justify-content-between py-0-5 py-sm-1 px-0-625 px-sm-1-25 px-xl-2 py-xl-1 disabled d-none d-xl-flex">
-        <NuxtLink to="/working">Working page</NuxtLink>
-      </i>
+    <div class="p-10 page bg-white overflow-hidden">
+      <div class="synchronized-scroll horizontal">
+        <i class="navigation-links align-items-center justify-content-between py-0-5 py-sm-1 px-0-625 px-sm-1-25 px-xl-2 py-xl-1 disabled d-none d-xl-flex">
+          <NuxtLink to="/">Home </NuxtLink>
+        </i>
+        <i class="navigation-links align-items-center justify-content-between py-0-5 py-sm-1 px-0-625 px-sm-1-25 px-xl-2 py-xl-1 disabled d-none d-xl-flex">
+          <NuxtLink to="/working">Working page</NuxtLink>
+        </i>
+      </div>
     </div>
     <slot />
   </div>
